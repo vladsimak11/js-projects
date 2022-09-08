0fa69ef0f0e43c840cab79c754e607fb1dd1571f
Vladyslav Simak <vladsima19@ukr.net>
2022-09-08 18:15:40 +0300

    version 1

---
+++ Board/app.js
@@ -0,0 +1,28 @@
+const board = document.querySelector('#board')
+const colors = ['#F5330A', '#F8E503', '#22F20D', '#08F7E9', '#B64BE1', '#F30A1C']
+const SQUARES_NUMBER = 600
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () => setColor(square))
+  square.addEventListener('mouseleave', () => removeColor(square))
+
+  board.append(square)
+}
+
+function setColor(element) {
+const color = getRandomColor()  
+element.style.backgroundColor = color
+element.style.boxShadow =  `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d'
+  element.style.boxShadow =  `0 0 2px #000`
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
